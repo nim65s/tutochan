c84243a2b17e42b3cde86072c1068a55772eb081
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-09-07 00:06:40 +0200

    ndh

--- tutochan/settings.py
+++ tutochan/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    PROJECT,
+    'ndh',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'channels',
     'chat',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
