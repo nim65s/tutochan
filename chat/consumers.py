aaecfcde792bbaa725e9120f8eab72d7a4ed2f72
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-09-06 14:40:34 +0200

    chat, tuto 3

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,28 +1,25 @@
 import json
 
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         # print(f'{self.scope=}')
         # print(f'{self.channel_name=}')
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'room_{self.room_name}'
-        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
-        self.accept()
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        async_to_sync(self.channel_layer.group_send)(self.room_group_name, {
-            'type': 'chati_message',
-            'message': message
-        })
+        await self.channel_layer.group_send(self.room_group_name, {'type': 'chati_message', 'message': message})
 
-    def chati_message(self, event):
-        self.send(text_data=json.dumps({'message': event['message']}))
+    async def chati_message(self, event):
+        await self.send(text_data=json.dumps({'message': event['message']}))
